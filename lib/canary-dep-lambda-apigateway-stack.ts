830a9b0b942258477a5a0555626855c73a28fabd
psreeteja <psteja02@gmail.com>
2025-08-17 22:27:07 +0530

    added infra code for canary deployment

--- lib/canary-dep-lambda-apigateway-stack.ts
+++ lib/canary-dep-lambda-apigateway-stack.ts
@@ -1,16 +1,54 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 
 export class CanaryDepLambdaApigatewayStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda function
+    const lambdaFn = new lambda.Function(this, 'CanaryDemoLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'handler.handler',
+      code: lambda.Code.fromAsset('src'),
+      description: 'Lambda for canary deployment demo',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CanaryDepLambdaApigatewayQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda alias for deployment
+    const lambdaAlias = new lambda.Alias(this, 'CanaryDemoLambdaAlias', {
+      aliasName: 'Prod',
+      version: lambdaFn.currentVersion,
+    });
+
+    // API Gateway REST API
+    const api = new apigateway.RestApi(this, 'CanaryDemoApi', {
+      restApiName: 'CanaryDemoApi',
+      deployOptions: {
+        stageName: 'prod',
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+      },
+    });
+    const rootIntegration = new apigateway.LambdaIntegration(lambdaAlias);
+    api.root.addMethod('GET', rootIntegration);
+
+    // CloudWatch alarm for Lambda errors
+    const errorAlarm = new cloudwatch.Alarm(this, 'LambdaErrorAlarm', {
+      metric: lambdaFn.metricErrors(),
+      threshold: 1,
+      evaluationPeriods: 1,
+      alarmDescription: 'Alarm for Lambda function errors',
+      actionsEnabled: true,
+    });
+
+    // CodeDeploy canary deployment
+    new codedeploy.LambdaDeploymentGroup(this, 'CanaryDeploymentGroup', {
+      alias: lambdaAlias,
+      deploymentConfig: codedeploy.LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+      alarms: [errorAlarm],
+    });
   }
 }
